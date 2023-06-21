7c2d5ce8401f929d44a618f339e691c5bebeb912
Abenezer <abenezersoccer@gmail.com>
2023-06-20 22:18:39 -0400

    added new categories and worked on search and sort

--- trees/binarysearch.py
+++ trees/binarysearch.py
@@ -1,14 +1,54 @@
-from typing import TypeVar, Generic
+import queue
 
-T = TypeVar('T')
-
-class NodeTree(Generic[T]):
-    def __init__(self)->None:
-        self.data = date
+class NodeTree:
+    def __init__(self, data:int)->None:
+        self.data = data
         self.left = None
         self.right = None
 
-class BinarySearchTree(Generic[T]):
-    def __init__(self, data:T)->None
-        self.root = 
+class BinarySearchTree:
+    def __init__(self, data:int)->None:
+        self.root = NodeTree(data)
 
+    def add(self, data:int)->None:
+        self.helper_addTree(self.root, None, data)
+
+    def helper_addTree(self, root:NodeTree, parent:NodeTree, data:int)->None:
+        if root == None:
+            node = NodeTree(data)
+            if data > parent.data:
+                parent.right = node
+            elif data < parent.data:
+                parent.left = node
+            return
+        if data > root.data:        
+            self.helper_addTree(root.right, root, data)
+        elif data < root.data:
+            self.helper_addTree(root.left, root, data)
+
+    def print_levelorder(self):
+        bus = queue.Queue(maxsize=0)
+        bus.put(self.root)
+        while not bus.empty():
+            root = bus.get()
+            if root != None:
+                print(root.data, end=" ")
+                bus.put(root.left)
+                bus.put(root.right)
+            else:
+                print(root, end=" ")
+
+
+
+def main():
+    binarytree = BinarySearchTree(5)
+    binarytree.add(2)
+    binarytree.add(1)
+    binarytree.add(3)
+    binarytree.add(4)
+    binarytree.add(7)
+    binarytree.add(8)
+    binarytree.add(6)
+    binarytree.print_levelorder()
+
+main()
