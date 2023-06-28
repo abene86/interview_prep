bc03e45693c001367eb6fae717f5e94fbcbd3275
Abenezer <abenezersoccer@gmail.com>
2023-06-28 16:23:49 -0400

    adding javascript folder to study guide

---
+++ javascript/review.js
@@ -0,0 +1,15 @@
+word ="Abenezer is a great person";
+
+for(let index =0; index < word.length; index++){
+        console.log(word.charAt(index));
+}
+
+
+function print_based_on_letter(letter){
+        lower_case = 
+        switch(letter){
+                case 'A':
+
+        }
+
+}
